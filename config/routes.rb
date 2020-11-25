29511420e0a3c10e483bfeec8eed68f9a068380f
valeria furin <v.furin@web.de>
2020-11-25 13:37:35 +0100

    added styling for regsiter form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  get '/settings', to: 'pages#settings'
-  patch '/settings', to: 'users#toggle_voice', as: :toggle
-  post '/settings', to: 'users#message', as: :message
+  root to: "pages#home"
+  get "/settings", to: "pages#settings"
+  patch "/settings", to: "users#toggle_voice", as: :toggle
+  post "/settings", to: "users#message", as: :message
   resources :contacts, only: [:new, :create, :edit, :update, :destroy]
-
 end
