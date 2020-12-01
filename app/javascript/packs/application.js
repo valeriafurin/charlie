50652c040b98d7b2dd1c0ee14d97c38c0dc756cc
Lea Schumacher <miss6muffet@gmail.com>
2020-12-01 14:59:41 +0100

    hide js function of the demo button done, fixed animation position

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("chartkick")
-require("chart.js")
-
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("chartkick");
+require("chart.js");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -32,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { enableToggleHelp } from "../scripts/get_help";
 import { setShowBtn } from "../scripts/btn-hidden";
+import { setHideBtn } from "../scripts/demo-btn";
 
 document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
   setShowBtn();
   enableToggleHelp();
+  setHideBtn();
 });
