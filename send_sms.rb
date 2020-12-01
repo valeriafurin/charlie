201b2a96f0c2ade7fa0bbfcae5de24aa578d5527
valeria furin <v.furin@web.de>
2020-12-01 16:03:14 +0100

    further implementations with twilio

--- send_sms.rb
+++ send_sms.rb
@@ -1,8 +1,8 @@
 # Download the twilio-ruby library from twilio.com/docs/libraries/ruby
 require "twilio-ruby"
 
-account_sid = "ACefb69d85b304fafeb67f04eb617e8416"
-auth_token = "97eaf829884c41cd267a344dc143a655"
+account_sid = ENV["TWILIO_ACCOUNT_SID"]
+auth_token = ENV["TWILIO_AUTH_TOKEN"]
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = "+12512913707" # Your Twilio number
@@ -11,5 +11,5 @@ to = "+491715544394" # Your mobile phone number
 client.messages.create(
   from: from,
   to: to,
-  body: "Hey friend, this is Twilio!",
+  body: "Hey there, this is Twilio!",
 )
